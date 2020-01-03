69fdf3cfc47614521c612849b21da6883059c464
fatlard1993 <fatlard1993@gmail.com>
2020-01-03 00:24:54 -0800

    add fabric templates

---
+++ data/fabric/Main.java
@@ -0,0 +1,10 @@
+package ${user}.${maven};
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Loaded ${name}");
+	}
+}
