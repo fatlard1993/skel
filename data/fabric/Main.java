7d7589cc75687a4b4f92c4864361fc25bcbe2618
fatlard1993 <fatlard1993>
2020-06-02 15:35:54 -0700

    cleanup

--- data/fabric/Main.java
+++ data/fabric/Main.java
@@ -6,7 +6,7 @@ public class Main implements ModInitializer {
 	public static final String MOD_ID = "${id}";
 
 	@Override
-	public void onInitialize() {
+	public void onInitialize(){
 		System.out.println("Loaded ${name}");
 	}
 }
